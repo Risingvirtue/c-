bfb547b834440e5bcaebea9bd05df2f646fac142
Johnny <risingvirtue@gmail.com>
2017-10-17 20:34:48 -0700

    linked List

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -21,15 +21,14 @@ struct node* newNode(int body, struct node* next) {
 
 int size(struct node* n) {
     struct node* pointer = n;
-    int counter;
-    counter = 0;
-    int* cp = &counter;
+    int count;
+    count = 0;
     while (pointer != NULL) {
-
-        *cp = *cp + 1;
+        count++;
         pointer = pointer->next;
     }
-    return counter;
+
+    return count;
 }
 
 int add(struct node* currNode, int a) {
@@ -48,7 +47,13 @@ int add(struct node* currNode, int a) {
         return 1;
     }
 }
+//must pass in pointer to pointer
+struct node* addFirst(struct node* currNode, int a) {
+    struct node* tempNode = newNode(a, currNode);
+    currNode = tempNode;
+    return currNode;
 
+}
 
 void printList(struct node* n) {
     printf("(");
@@ -60,15 +65,63 @@ void printList(struct node* n) {
     }
 
     printf(")\n");
-
 }
 
+int removeFirst(struct node** currNode) {
+    int answer = (*currNode)->body;
+    *currNode = ((*currNode)->next);
+    printf("%d", answer);
+    return answer;
+}
+
+int removeIndex(struct node** currNode, int index) {
+    int answer;
+    if (index > size(*currNode) || index < 0) {
+        return 0;
+    }
+    struct node* tempPoint = *currNode;
+    if (index == 0) {
+        *currNode = (*currNode)->next;
+    }
+    struct node* prevNode = tempPoint;
+    int i;
+    for (i = 0; i < index; i++) {
+        prevNode = tempPoint;
+        tempPoint = tempPoint->next;
+    }
+    answer = tempPoint->body;
+    prevNode->next = tempPoint->next;
+    return answer;
+}
+
+int peek(struct node* currNode) {
+    return currNode->body;
+}
+
+array* toArray(struct node* currNode) {
+    int numElle;
+    numElle = size(currNode);
+    int n[numElle];
+    int i;
+
+    for (i = 0; i < numElle; i++) {
+        n[i] = currNode->body;
+        currNode = currNode->next;
+    }
+    return &n;
+
+}
 int main() {
     struct node* newList = newNode(2, NULL);
-    printList(newList);
+   // printList(newList);
     add(newList, 3);
-    printList(newList);
+    //printList(newList);
     int sizeNode = size(newList);
-    printf(sizeNode);
+    //printf("%d", sizeNode);
+    newList = addFirst(newList, 14);
+    printList(newList);
+    printf("%d\n", removeIndex(&newList, 2));
+    printList(newList);
+
     return 0;
 }
